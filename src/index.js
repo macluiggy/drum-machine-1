8aa6b37d4fed467766c4b841c0551001e63344b4
macluiggy <ferrinluiggy@gmail.com>
2021-08-23 10:21:39 -0500

    ddkkdkdkdkd

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "../src/components/App/App"
-//hola, solo estoy aprendiendo a usar git para contribuir, no tienes que fisonar estos cambiosss
+//hola, solo estoy aprendiendo a usar git para contribuir, no tienes que fisonar estos cambiossssss
 ReactDOM.render(<App />, document.getElementById("root"))
