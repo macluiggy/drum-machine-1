5f57da98a6e6029b5fd201eb53c9b1f06402ec0d
macluiggy <ferrinluiggy@gmail.com>
2021-08-23 10:55:20 -0500

    djjd

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "../src/components/App/App"
-//hola, solo estoy aprendiendo a usar git para contribuir, no tienes que fisonar estos cambios xd
+//hola, solo estoy aprendiendo a usar git para contribuir, no tienes que fisonar estos cambios xdd
 //contenido que va a ser derivado en una rama local pero con un nombre diferente
 ReactDOM.render(<App />, document.getElementById("root"))
