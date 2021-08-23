02af64bd8cb069021f7ff498e4bba2d9bc63b76b
Luiggy Macias <74107886+macluiggy@users.noreply.github.com>
2021-08-23 10:41:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "../src/components/App/App"
 //hola, solo estoy aprendiendo a usar git para contribuir, no tienes que fisonar estos cambios xd
+//contenido que va a ser derivado en una rama local pero con un nombre diferente
 ReactDOM.render(<App />, document.getElementById("root"))
