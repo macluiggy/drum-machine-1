a9ac8fccd2c5644b9e5d8e90aa2a44da81358d01
macluiggy <ferrinluiggy@gmail.com>
2021-08-23 09:42:23 -0500

    changes made

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "../src/components/App/App"
-
+//hola, solo estoy aprendiendo a usar git para contribuir, no tienes que fisonar estos cambios
 ReactDOM.render(<App />, document.getElementById("root"))
